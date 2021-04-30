133febbe67d5873e9ea692c27f0db8cefab7cb3c
Nadno <eunadno@gmail.com>
2021-04-30 17:32:24 -0300

    create a abstract class Component that create elements and require a render method

---
+++ src/views/Component.ts
@@ -0,0 +1,28 @@
+export default abstract class Component {
+  public static createElement<K extends keyof HTMLElementTagNameMap>(
+    name: K,
+    content: string | Array<HTMLElement> = '',
+    attrs?: Record<string, string>
+  ): HTMLElementTagNameMap[K] {
+    const $el: any = document.createElement(name);
+
+    if (typeof content == 'string') {
+      $el.insertAdjacentText('beforeend', content);
+    } else {
+      content.forEach($content =>
+        $el.insertAdjacentElement('beforeend', $content)
+      );
+    }
+
+    if (!attrs) return $el;
+    const attrsAsArray = Object.entries(attrs);
+
+    const setElementAttrs = ([key, value]: [string, string]) =>
+      $el.setAttribute(key, value);
+    attrsAsArray.forEach(setElementAttrs);
+
+    return $el;
+  }
+
+  public abstract render(): HTMLElement;
+}
