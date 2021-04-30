e49cf8a2df5e2c5ab7f96345780a90ffda3612e7
Nadno <eunadno@gmail.com>
2021-04-30 19:53:43 -0300

    implement contact settings functionality, using Settings component, and adding click event to the container element

--- src/views/Contact/Contacts.ts
+++ src/views/Contact/Contacts.ts
@@ -1,5 +1,6 @@
 import Contact from '.';
 import Component from '../component';
+import Settings from './Settings';
 import ContactList from '../../models/ContactList';
 
 import { AppContext } from '../../App';
@@ -8,6 +9,8 @@ export default class Contacts extends Component {
   private contacts: ContactList;
 
   private $container: HTMLElement;
+  private $settings: HTMLElement;
+  private $settingPosition: HTMLElement | null = null;
 
   constructor({ contacts }: AppContext) {
     super();
@@ -18,8 +21,61 @@ export default class Contacts extends Component {
     });
 
     this.contacts = contacts;
+    this.$settings = new Settings([
+      { href: '/edit', label: 'Editar' },
+      { href: '/remove', label: 'Apagar' },
+    ]).render();
 
     this.appendContactList = this.appendContactList.bind(this);
+    this.handleToggleSettings = this.handleToggleSettings.bind(this);
+  }
+
+  public turnSettingPositionOff() {
+    if (this.$settingPosition) {
+      this.$settingPosition.setAttribute('aria-checked', 'false');
+      this.$settingPosition.classList.remove('on');
+      this.$settingPosition = null;
+    }
+  }
+
+  public handleToggleSettings(e: Event) {
+    let target = e.target as HTMLElement;
+    const parent = target.parentNode as HTMLElement;
+
+    if (parent && parent.matches('.contact__edit')) {
+      target = parent;
+    }
+
+    if (target.matches('.contact__edit')) {
+      if (target.matches('.on')) {
+        const turnSettingsOff = () => {
+          this.$settings.remove();
+          this.$settings.removeEventListener('transitionend', turnSettingsOff);
+        };
+        this.$settings.addEventListener('transitionend', turnSettingsOff);
+
+        this.$settings.setAttribute('aria-hidden', 'true');
+        this.turnSettingPositionOff();
+        target.classList.remove('on');
+
+        return;
+      }
+
+      if (this.$settingPosition) {
+        this.turnSettingPositionOff();
+      }
+
+      this.$settingPosition = target;
+      this.$settingPosition.insertAdjacentElement('beforeend', this.$settings);
+
+      this.$settings.setAttribute('aria-hidden', 'false');
+      this.$settingPosition.setAttribute('aria-checked', 'true');
+
+      const turnSettingsPositionOn = () =>
+        (this.$settingPosition as HTMLElement).classList.add('on');
+
+      setTimeout(turnSettingsPositionOn, 100);
+    }
   }
 
   private createLetterList(letter: string) {
@@ -68,6 +124,7 @@ export default class Contacts extends Component {
   public render(): HTMLElement {
     setTimeout(this.appendContactList, 500);
 
+    this.$container.addEventListener('click', this.handleToggleSettings);
     return this.$container;
   }
 }
