4dccef321aa1eb8875f984089b428dcc11fbc507
Nadno <eunadno@gmail.com>
2021-05-05 19:18:16 -0300

    refactor(Contacts Component): extract Settings event functionality, and publishes forEachContact event

--- src/views/Contact/Contacts.ts
+++ src/views/Contact/Contacts.ts
@@ -1,94 +1,37 @@
 import Contact from '.';
 import Component from '../component';
 import Settings from './Settings';
-import ContactList from '../../models/ContactList';
 
 import { AppContext } from '../../App';
+import { IObserver } from '../../controllers/Observer/types';
+import { IContact } from '../../models/ContactList/types';
 
 export default class Contacts extends Component {
-  private contacts: ContactList;
+  private emitter: IObserver;
+  private settings: Settings;
 
   private $container: HTMLElement;
-  private $settings: HTMLElement;
-  private $settingPosition: HTMLElement | null = null;
 
-  constructor({ contacts }: AppContext) {
+  constructor({ emitter }: AppContext) {
     super();
+
+    this.emitter = emitter;
+    this.settings = new Settings([
+      { href: '/edit', label: 'Editar' },
+      { href: '/remove', label: 'Apagar' },
+    ]);
 
     this.$container = Component.createElement('ol', [], {
       class: 'contacts',
       type: 'A',
     });
 
-    this.contacts = contacts;
-    this.$settings = new Settings([
-      { href: '/edit', label: 'Editar' },
-      { href: '/remove', label: 'Apagar' },
-    ]).render();
-
     this.appendContactList = this.appendContactList.bind(this);
-    this.handleToggleSettings = this.handleToggleSettings.bind(this);
   }
 
-  private setButtonAriaChecked(bool: string) {
-    if (this.$settingPosition) {
-      const $button = this.$settingPosition.querySelector('.settings-button');
-      if ($button) $button.setAttribute('aria-checked', bool);
-    }
-  }
-
-  public turnSettingPositionOff() {
-    if (this.$settingPosition) {
-      this.setButtonAriaChecked('false');
-
-      this.$settingPosition.classList.remove('on');
-      this.$settingPosition = null;
-    }
-  }
-
-  public handleToggleSettings(e: Event) {
-    let target = e.target as HTMLElement;
-    const parent = target.parentNode as HTMLElement;
-
-    if (parent && parent.matches('.settings-button')) {
-      target = parent;
-    }
-
-    if (target.matches('.settings-button')) {
-      if (target.matches('[aria-checked="true"]')) {
-        const turnSettingsOff = () => {
-          this.$settings.remove();
-          this.$settings.removeEventListener('transitionend', turnSettingsOff);
-        };
-
-        this.$settings.addEventListener('transitionend', turnSettingsOff);
-        this.$settings.setAttribute('aria-hidden', 'true');
-        this.turnSettingPositionOff();
-
-        return;
-      }
-
-      if (this.$settingPosition) {
-        this.turnSettingPositionOff();
-      }
-
-      this.$settingPosition = target.parentNode as HTMLElement;
-      this.$settingPosition.appendChild(this.$settings);
-
-      this.$settings.setAttribute('aria-hidden', 'false');
-      this.setButtonAriaChecked('true');
-
-      const turnSettingsPositionOn = () =>
-        (this.$settingPosition as HTMLElement).classList.add('on');
-
-      setTimeout(turnSettingsPositionOn, 100);
-    }
-  }
-
-  private createLetterList(letter: string) {
+  private createContactList(letter: string) {
     const $list = Component.createElement('ol', '', {
       type: 'a',
-      'data-letter': letter,
     });
 
     const $listLabel = Component.createElement('span', letter.toUpperCase(), {
@@ -109,28 +52,29 @@ export default class Contacts extends Component {
   }
 
   private appendContactList() {
-    let currentLetter;
-    let $contactsList;
+    let currentLetter: string;
+    let $contactsList: HTMLElement;
 
-    for (const { contact, letter, index } of this.contacts) {
-      if (letter !== currentLetter) {
-        currentLetter = letter;
-        $contactsList = this.createLetterList(letter);
+    this.emitter.emit(
+      'forEachContact',
+      (contact: IContact, letterKey: string, index: number) => {
+        if (letterKey !== currentLetter) {
+          currentLetter = letterKey;
+          $contactsList = this.createContactList(letterKey);
+        }
+
+        const contactId = `${letterKey}-${index}`;
+        const $contact = new Contact(contact.name, contactId);
+
+        $contactsList.appendChild($contact.render());
       }
-
-      const contactId = String(index);
-
-      const $contact = new Contact(contact.name, contactId);
-      ($contactsList as HTMLElement).insertAdjacentElement(
-        'beforeend',
-        $contact.render()
-      );
-    }
+    );
   }
 
   public render(): HTMLElement {
-    setTimeout(this.appendContactList, 1000);
-    this.$container.addEventListener('click', this.handleToggleSettings);
+    setTimeout(this.appendContactList, 200);
+
+    this.$container.addEventListener('click', this.settings.handleClick);
     return this.$container;
   }
 }
