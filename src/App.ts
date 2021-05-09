7602edbb9d5185133a1d186d77e3347907267246
Nadno <eunadno@gmail.com>
2021-05-09 15:33:27 -0300

    refactor(App): move contact list subscribe events to FactoryContactList

--- src/App.ts
+++ src/App.ts
@@ -1,30 +1,23 @@
-import emitter from './controllers/Observer';
-import ContactList from './models/ContactList';
-import Router from './controllers/Router';
 import Render from './views/Render';
+import Router from './controllers/Router';
+import Observer from './controllers/Emitter';
 
 import Home from './views/routes/Home/Home';
-import EditContact from './views/routes/Edit';
-import CreateContact from './views/routes/Create';
+import EditContact from './views/routes/Contact/Edit';
+import CreateContact from './views/routes/Contact/Create';
 
-import StringUtil from './utils/StringUtil';
-
-import {
-  ContactAndPosition,
-  IContact,
-  IContactsList,
-} from './models/ContactList/types';
-import { IObserver } from './controllers/Observer/types';
-import { ILinkedList } from './models/LinkedList/types';
+import { IEmitter } from './controllers/Emitter/types';
+import { IContactsList } from './models/ContactList/types';
+import FactoryContactList from './models/ContactList/FactoryContactList';
 
 export interface AppContext {
   router: Router;
-  emitter: IObserver;
+  emitter: IEmitter;
 }
 
 export default class App {
   constructor(
-    private emitter: IObserver,
+    private emitter: IEmitter,
     private contacts: IContactsList,
     private router: Router,
     private render: Render
@@ -34,7 +27,9 @@ export default class App {
 
   public static createApp(): App {
     const $app = document.getElementById('app') || document.body;
-    const contacts = new ContactList(new StringUtil());
+
+    const emitter = new Observer();
+    const contacts = FactoryContactList.createContactList(emitter);
 
     const router = new Router(location, emitter);
     $app.addEventListener('click', router.handleLinkClick);
@@ -45,7 +40,6 @@ export default class App {
 
   public start(): void {
     this.setRoutes();
-    this.subscribeContactListEvents();
 
     this.router.renderPath();
     window.onpopstate = () => this.router.goTo(location.pathname);
@@ -55,24 +49,5 @@ export default class App {
     this.router.path('/', ctx => new Home(ctx).render());
     this.router.path('/edit', ctx => new EditContact(ctx).render());
     this.router.path('/create', ctx => new CreateContact(ctx).render());
-  }
-
-  private subscribeContactListEvents() {
-    const { emitter, contacts } = this;
-
-    const forEachContactList = (
-      cb: (contacts: ILinkedList<IContact>, letterKey: string) => any
-    ) => contacts.forEachList(cb);
-    emitter.on('forEachContactList', forEachContactList);
-
-    const createContact = (contact: IContact) => {
-      contact.createdAt = new Date().toLocaleDateString('pt-BR');
-      contacts.createContact(contact);
-    };
-    emitter.on('createContact', createContact);
-
-    const editContact = ({ contact, letterKey, index }: ContactAndPosition) =>
-      contacts.editContact(contact, { letterKey, index });
-    emitter.on('editContact', editContact);
   }
 }
