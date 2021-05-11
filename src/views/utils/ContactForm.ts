dd53dc60cb07a2d91d04c162c1e62142653fda94
Nadno <eunadno@gmail.com>
2021-05-11 18:41:15 -0300

    feat(view ContactForm): create contact form util

---
+++ src/views/utils/ContactForm.ts
@@ -0,0 +1,58 @@
+import Form from './Form';
+
+import '../../../public/styles/views/create-contact.scss';
+
+interface ContactFormData {
+  name: string;
+  tel: string;
+  [key: string]: string;
+}
+
+interface IContactForm {
+  submit: string;
+  formTitle: string;
+  initialData?: ContactFormData;
+  handleSubmit: (e: Event) => any;
+}
+
+export default class ContactForm {
+  public formElement: HTMLElement;
+  public data: ContactFormData;
+
+  constructor({
+    submit,
+    formTitle,
+    handleSubmit,
+    initialData = { name: '', tel: '' },
+  }: IContactForm) {
+    this.handleChange = this.handleChange.bind(this);
+
+    const { data, formElement } = new Form(initialData)
+      .setCurrentHandleChange(this.handleChange)
+      .createField(formTitle, [
+        {
+          id: 'name',
+          name: 'name',
+          label: 'Nome',
+          placeholder: 'Nome',
+        },
+        {
+          id: 'tel',
+          name: 'tel',
+          label: 'Número de telefone',
+          placeholder: 'ex.: 912345678',
+        },
+      ])
+      .createForm({ class: 'create-contact' }, submit, handleSubmit);
+
+    this.data = data;
+    this.formElement = formElement;
+  }
+
+  private handleChange(e: Event): void {
+    const { name, value } = e.target as HTMLInputElement;
+    const { data } = this;
+
+    if (name in data) data[name] = value;
+  }
+}
