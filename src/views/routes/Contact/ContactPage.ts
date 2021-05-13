6938512973c930da873eeedf9bed33cf98cd5c52
Nadno <eunadno@gmail.com>
2021-05-13 11:11:28 -0300

    feat(view ContactPage): implement contact page and its method: getContact

---
+++ src/views/routes/Contact/ContactPage.ts
@@ -0,0 +1,28 @@
+import { AppContext, AppState } from '../../../App';
+import { IContact } from '../../../models/ContactList/types';
+
+import PageComponent from '../PageComponent';
+
+export default abstract class ContactPage extends PageComponent {
+  constructor(protected ctx: AppContext<AppState>) {
+    super();
+  }
+
+  protected getContact(): IContact {
+    const { state, router } = this.ctx;
+
+    const { id } = router.getParams();
+    const [key, index] = id.split('-');
+
+    let result = { name: '', tel: '', createdAt: '' };
+    if (!key || !index) return result;
+
+    const contact = state.contacts.getContact(key, Number(index));
+    if (!contact) return result;
+
+    const { name = '', tel = '' } = contact.value;
+    Object.assign(result, { name, tel });
+
+    return result;
+  }
+}
