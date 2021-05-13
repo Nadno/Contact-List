6d6d35b7f92e46b751120d45dede2e92f0fc97fa
Nadno <eunadno@gmail.com>
2021-05-13 15:11:41 -0300

    improvement(PageComponent): create a PageConstructor interface

--- src/views/routes/PageComponent.ts
+++ src/views/routes/PageComponent.ts
@@ -1,7 +1,12 @@
+import { AppContext } from '../../App';
 import Component from '../component';
 
-interface PageComponent {
+interface PageComponent extends Component<HTMLElement[]> {
   unMount?(): void;
+}
+
+export interface PageConstructor extends PageComponent {
+  new (ctx: AppContext): PageComponent;
 }
 
 abstract class PageComponent extends Component<HTMLElement[]> {
