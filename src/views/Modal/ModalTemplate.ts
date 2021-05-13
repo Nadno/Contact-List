84cd02d06d830f91cbcafe8d4f37a6e49448a05e
Nadno <eunadno@gmail.com>
2021-05-13 11:09:59 -0300

    feat(view ModalTemplate): define a default modal template

---
+++ src/views/Modal/ModalTemplate.ts
@@ -0,0 +1,23 @@
+import Modal from '.';
+import Component from '../component';
+
+export default class ModalTemplate {
+  public static defaultTemplate<M extends Modal>(modal: M): M {
+    const $content = Component.createElement('p', '', {
+      className: 'modal__content',
+    });
+
+    const $hr = Component.createElement('hr');
+
+    const $buttons = Component.createElement('div', '', {
+      className: 'modal__buttons',
+    });
+
+    modal
+      .addElement($content, 'content')
+      .addElement($hr)
+      .addElement($buttons, 'buttons');
+
+    return modal;
+  }
+}
