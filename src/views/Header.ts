16091222d3e1d8a22a407aaceffc67b780502727
Nadno <eunadno@gmail.com>
2021-05-13 10:58:35 -0300

    refactor(view Header): add app context to Contacts component

--- src/views/Header.ts
+++ src/views/Header.ts
@@ -35,7 +35,7 @@ export default class Header extends Component {
       }
     );
 
-    this.contactsResult = new Contacts({
+    this.contactsResult = new Contacts(ctx, {
       className: 'contacts search-result',
       type: 'A',
     });
