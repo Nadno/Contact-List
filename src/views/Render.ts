28e769103523de011616ef4439b02a49b7e43787
Nadno <eunadno@gmail.com>
2021-05-13 10:50:11 -0300

    refactor(view Render): implement the new view return and PageComponent interface with unMount method

--- src/views/Render.ts
+++ src/views/Render.ts
@@ -1,16 +1,20 @@
 import NotFound from './routes/404';
+import PageComponent from './routes/PageComponent';
 
 export default class Render {
+  protected currentPage: PageComponent;
+
   constructor(
-    private routeFallback: () => HTMLElement[],
+    private routeFallback: () => PageComponent,
     private rootElement: HTMLElement,
     private ctx: any
   ) {
+    this.currentPage = routeFallback();
     this.renderRoute = this.renderRoute.bind(this);
   }
 
   public static createRender(rootElement: HTMLElement, ctx: any): Render {
-    const routeFallback = () => new NotFound().render();
+    const routeFallback = () => new NotFound();
     return new Render(routeFallback, rootElement, ctx);
   }
 
@@ -20,8 +24,11 @@ export default class Render {
   }
 
   public renderRoute(
-    view: (ctx: any) => HTMLElement[] = this.routeFallback
+    view: (ctx: any) => PageComponent = this.routeFallback
   ): void {
-    this.renderElements(view(this.ctx));
+    if (this.currentPage.unMount) this.currentPage.unMount();
+
+    this.currentPage = view(this.ctx);
+    this.renderElements(this.currentPage.render());
   }
 }
