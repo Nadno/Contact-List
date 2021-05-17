4fdcbb400436ad7c2edb5dcdc0099cd75a4d6de8
Nadno <eunadno@gmail.com>
2021-05-17 14:59:25 -0300

    feat(emitter): add remove event

--- src/controllers/Emitter/types.d.ts
+++ src/controllers/Emitter/types.d.ts
@@ -1,4 +1,5 @@
-export interface IObserver {
+export interface IEmitter {
   on(event: string, cb: (...args: any) => void): void;
   emit(event: string, ...args: any): void;
+  remove(event: string, excludeFunc: Function): void;
 }
