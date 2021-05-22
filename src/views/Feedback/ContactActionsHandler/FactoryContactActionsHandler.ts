1cc8bab3e7999fd70a5faf7683c715d337d23595
Nadno <eunadno@gmail.com>
2021-05-22 00:41:48 -0300

    feat(FactoryContactActionsHandler): add handlers to contact list events

---
+++ src/views/Feedback/ContactActionsHandler/FactoryContactActionsHandler.ts
@@ -0,0 +1,22 @@
+import ContactListEmitter from '../../../models/ContactListEmitter';
+import StringUtil from '../../../utils/StringUtil';
+import NotifyList from '../Notifies';
+import ContactActionsHandler from '.';
+
+import { IContactsList } from '../../../models/ContactList/types';
+import { IEmitter } from '../../../controllers/Emitter/types';
+
+export default class FactoryContactActionsHandler {
+  public static createContactList(emitter: IEmitter, notifyList: NotifyList): IContactsList {
+    const contactList = new ContactListEmitter(emitter, new StringUtil());
+    const contactActionsHandler = new ContactActionsHandler(
+      emitter,
+      notifyList,
+      contactList
+    );
+
+    emitter.on('contactCreated', contactActionsHandler.handleCreate);
+
+    return contactList;
+  }
+}
