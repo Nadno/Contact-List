a9cf3a6f768cf566c663fce2ce944872322708c5
Nadno <eunadno@gmail.com>
2021-05-26 00:26:34 -0300

    feat(FactoryContactActionsHandler): add contacts params & implement handleEditContact

--- src/views/Feedback/ContactActionsHandler/FactoryContactActionsHandler.ts
+++ src/views/Feedback/ContactActionsHandler/FactoryContactActionsHandler.ts
@@ -3,12 +3,20 @@ import StringUtil from '../../../utils/StringUtil';
 import NotifyList from '../Notifies';
 import ContactActionsHandler from '.';
 
-import { IContactsList } from '../../../models/ContactList/types';
+import { IContact, IContactsList } from '../../../models/ContactList/types';
 import { IEmitter } from '../../../controllers/Emitter/types';
 
 export default class FactoryContactActionsHandler {
-  public static createContactList(emitter: IEmitter, notifyList: NotifyList): IContactsList {
-    const contactList = new ContactListEmitter(emitter, new StringUtil());
+  public static createContactList(
+    emitter: IEmitter,
+    notifyList: NotifyList,
+    contacts?: IContact[]
+  ): IContactsList {
+    const contactList = new ContactListEmitter(
+      emitter,
+      new StringUtil(),
+      contacts
+    );
     const contactActionsHandler = new ContactActionsHandler(
       emitter,
       notifyList,
@@ -17,6 +25,7 @@ export default class FactoryContactActionsHandler {
 
     emitter.on('contactCreated', contactActionsHandler.handleCreateContact);
     emitter.on('contactDeleted', contactActionsHandler.handleDeleteContact);
+    emitter.on('contactEdited', contactActionsHandler.handleEditContact);
 
     return contactList;
   }
