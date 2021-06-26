bee9a411c5925e7f04c4421e4d97fa3132b9cf78
Nadno <eunadno@gmail.com>
2021-06-26 14:15:47 -0300

    adjust(Emitter): adjust to implement breaking changes on linked list

--- src/controllers/Emitter/index.ts
+++ src/controllers/Emitter/index.ts
@@ -1,41 +1,41 @@
-import LinkedList from '../../models/LinkedList';
+import LinkedArray from '../../models/LinkedList/LinkedArray';
 
 import { IEmitter } from './types';
-import { ILinkedList } from '../../models/LinkedList/types';
+import { ILinkedArray } from '../../models/LinkedList/types';
 
 class Emitter implements IEmitter {
-  private events: Record<string, ILinkedList<Function>> = {};
+	private events: Record<string, ILinkedArray<Function>> = {};
 
-  public on(event: string, cb: (...args: any) => void): void {
-    if (event in this.events) {
-      this.events[event].push(cb);
-      return;
-    }
+	public on(event: string, cb: (...args: any) => void): void {
+		if (event in this.events) {
+			this.events[event].push(cb);
+			return;
+		}
 
-    this.events[event] = new LinkedList<Function>();
-    this.events[event].push(cb);
-  }
+		this.events[event] = new LinkedArray<Function>();
+		this.events[event].push(cb);
+	}
 
-  public emit(event: string, ...args: any): void {
-    if (!(event in this.events)) return;
-    this.events[event].forEach(cb => cb(...args));
-  }
+	public emit(event: string, ...args: any): void {
+		if (!(event in this.events)) return;
+		this.events[event].forEach((cb) => cb(...args));
+	}
 
-  public clear(event: string): boolean {
-    return delete this.events[event];
-  }
+	public clear(event: string): boolean {
+		return delete this.events[event];
+	}
 
-  public remove(event: string, excludeFunc: Function): void {
-    if (!(event in this.events)) return;
-    const list = this.events[event];
+	public remove(event: string, excludeFunc: Function): void {
+		if (!(event in this.events)) return;
+		const list = this.events[event];
 
-    for (const node of list) {
-      if (node.value === excludeFunc) {
-        list.remove(node);
-        break;
-      }
-    }
-  }
+		for (const node of list) {
+			if (node.value === excludeFunc) {
+				list.remove(node);
+				break;
+			}
+		}
+	}
 }
 
 export default Emitter;
