83877189bdcb68eabd7f25c4f8d821cabbb8d82a
Nadno <eunadno@gmail.com>
2021-06-26 14:25:39 -0300

    improvement(Notifies): implement LinkedQueue

--- src/views/Feedback/Notifies/index.ts
+++ src/views/Feedback/Notifies/index.ts
@@ -1,42 +1,44 @@
-import LinkedList from '../../../models/LinkedList';
+import LinkedQueue from '../../../models/LinkedList/LinkedQueue';
+import { ILinkedQueue } from '../../../models/LinkedList/types';
 import Notify, { NotifyComponents } from './Notify';
 
 interface NotificationObject extends Omit<NotifyComponents, 'where'> {}
 
 export default class NotifyList {
-  private queue = new LinkedList<NotificationObject>(10);
-  private $notify: HTMLElement;
-  private hasCurrent = false;
+	private queue: ILinkedQueue<NotificationObject> =
+		new LinkedQueue<NotificationObject>(10);
+	private $notify: HTMLElement;
+	private hasCurrent = false;
 
-  constructor(elementId: string) {
-    this.$notify = document.getElementById(elementId) || document.body;
-  }
+	constructor(elementId: string) {
+		this.$notify = document.getElementById(elementId) || document.body;
+	}
 
-  public showNotify(notify: NotificationObject): void {
-    Notify.showNotification(
-      {
-        ...notify,
-        where: this.$notify,
-      },
-      this.updateNotify
-    );
-    this.hasCurrent = true;
-  }
+	public showNotify(notify: NotificationObject): void {
+		Notify.showNotification(
+			{
+				...notify,
+				where: this.$notify,
+			},
+			this.updateNotify
+		);
+		this.hasCurrent = true;
+	}
 
-  public addNotify(notify: NotificationObject): void {
-    if (this.hasCurrent) {
-      this.queue.push(notify);
-      return;
-    }
+	public addNotify(notify: NotificationObject): void {
+		if (this.hasCurrent) {
+			this.queue.push(notify);
+			return;
+		}
 
-    this.showNotify(notify);
-  }
+		this.showNotify(notify);
+	}
 
-  public updateNotify = (): void => {
-    this.hasCurrent = false;
-    const next = this.queue.shift();
-    if (next) this.showNotify(next.value);
-  };
+	public updateNotify = (): void => {
+		this.hasCurrent = false;
+		const next = this.queue.shift();
+		if (next) this.showNotify(next.value);
+	};
 
-  public clear = (): void => this.queue.forEach(() => this.queue.shift());
+	public clear = (): void => this.queue.forEach(() => this.queue.shift());
 }
