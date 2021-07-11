99be43892d7c8da9de10a6a51e3c62c759208227
Nadno <eunadno@gmail.com>
2021-07-11 14:10:24 -0300

    feat(index): implement connection lost modal

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
 'use strict';
 
 import App from './App';
+import WarnModal from './views/Feedback/Modal/Warn';
+import Fallback from './views/Fallback';
+
 import './styles/main.scss';
+
+window.addEventListener('offline', alertConnectionLost);
+window.addEventListener('online', alertReconnection);
+
+function alertConnectionLost() {
+  WarnModal.warn({
+    message:
+      'Para acessar outras partes da aplicação é necessário a conexão com a internet, por favor espere sua conexão até acessar outras partes da aplicação!',
+    title: 'Conexão perdida',
+  });
+
+  Fallback.setFallbackView('ConnectionLost');
+}
+
+function alertReconnection() {
+  alert('Conexão restabelecida!');
+  Fallback.setFallbackView('NotFound');
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   try {
-    const app = App.createApp();
+    const app = App.createApp(document.getElementById('app'));
     app.start();
   } catch (err) {
     console.error(err);
